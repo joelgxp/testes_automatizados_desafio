947fa48cfb5a23742209e354c6a5bd17eef63324
Joel <joelvieirasouza@gmail.com>
2023-01-30 21:58:31 -0300

    inicial

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,36 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+
+    /**
+     *
+     * Um carro tem os seguintes atributos:
+     *  - cor
+     *  - marca
+     *  - modelo
+     *  - ligado
+     *  - velocidadeAtual
+     *  - velocidadeMáxima
+     *
+     *  Enquanto o carro estiver desligado deve ser capaz de:
+     *  - Ligar
+     *  - Mostrar estado atual
+     *
+     * Enquanto o carro estiver ligado deve ser capaz de:
+     * - Desligar
+     * - Acelerar
+     * - Frear
+     * - Motrar estado atual
+     *
+     * Regras:
+     *
+     * - Só podemos realizar as ações como acelerar e frear com o carro ligado
+     * - Só podemos desligar o carro quando ele parar totalmente (velocidadeAtual = 0)
+     * - Não existe velocidade negativa
+     * - O carro não pode passar de sua velocidade máxima
+     * - O carro inicia desligado
+     *
+     */
+
+    public static void main(String[] args) {
+
+    }
 }
--- src/test/java/service/CarroServiceTest.java
+++ src/test/java/service/CarroServiceTest.java
@@ -1,2 +1,51 @@
-package service;public class CarroService {
+package service;
+
+import model.Carro;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CarroServiceTest {
+    @Test
+    public void deveAcelerarCorretamtente() {
+        CarroService carroService = new CarroServiceImpl();
+
+        //dado
+        Carro carro = new Carro("azul", "fiat", "uno", 2020, 150);
+        carroService.ligar(carro);
+
+        //quando
+        carroService.acelerar(carro, 10);
+
+        //entao
+        Assert.assertEquals(10, carro.getVelocidadeAtual());
+    }
+
+    @Test
+    public void naoDevePassarDaVelocidadeMaxima (){
+        //dado
+        CarroService carroService = new CarroServiceImpl();
+        Carro carro = new Carro("azul", "fiat", "uno", 2020, 100);
+        carroService.ligar(carro);
+
+        //quando
+
+
+        //entao
+
+
+    }
+
+    @Test
+    public void umCarroDeveIniciarDesligado(){
+        CarroService carroService = new CarroServiceImpl();
+        Carro carro = new Carro("azul", "fiat", "uno", 2020, 100);
+        carroService.ligar(carro);
+
+        //entao
+
+        //Assert.assertTrue();
+        Assert.assertFalse(carro.isLigado());
+
+
+    }
 }
